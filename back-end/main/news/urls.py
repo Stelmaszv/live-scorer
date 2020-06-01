08955a3277f8807afc357f1fdef00af783cedfa4
Stelmaszv <stelmaszv@gmail.com>
2020-06-01 16:47:30 +0200

    add coment test

--- back-end/main/news/urls.py
+++ back-end/main/news/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path, include
 from . import views
 from rest_framework import routers
-
+##dqd
 router = routers.DefaultRouter()
 router.register('news',views.Top_News)
 urlpatterns = [

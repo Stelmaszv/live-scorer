6ef8943e148dee38fe304aa1ff9a5e878ace266a
Stelmaszv <stelmaszv@gmail.com>
2020-06-01 14:57:45 +0200

    add routing & comonents start & main

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './components/main/main.component';
+import { StartComponent } from './components/start/start.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'' , component:StartComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents=[MainComponent,StartComponent]

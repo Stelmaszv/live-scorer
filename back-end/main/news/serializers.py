f17f0ef72a8168c59c642369a4710a2593e76349
Stelmaszv <stelmaszv@gmail.com>
2020-06-01 14:31:19 +0200

    add view to url

--- back-end/main/news/serializers.py
+++ back-end/main/news/serializers.py
@@ -3,4 +3,4 @@ from .models import News
 class NewsSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model  = News
-        fields =  ('id','name','url')
+        fields =  ('id','title','url')

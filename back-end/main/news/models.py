27904e7e4831f314efdb1f0f2d4ea9bdbaf20626
Stelmaszv <stelmaszv@gmail.com>
2020-06-01 15:59:21 +0200

    update models.py (ontop)

--- back-end/main/news/models.py
+++ back-end/main/news/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 class News(models.Model):
     title = models.CharField(max_length=50)
+    ontop = models.BooleanField(default=False)
     def __str__(self):
         return  self.title

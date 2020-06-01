23d5b1227ab3c240741a9ee9e7d7c54cf55112e5
Stelmaszv <stelmaszv@gmail.com>
2020-06-01 14:27:54 +0200

    add serializers.py, add NewsSerializer and add view NewsView

---
+++ back-end/main/news/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+from .models import News
+class NewsSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model  = News
+        fields =  ('id','name','url')
--- back-end/main/news/views.py
+++ back-end/main/news/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
-# Create your views here.
+from rest_framework import viewsets
+from .models import News
+from .serializers import NewsSerializer
+class NewsView(viewsets.ModelViewSet):
+    queryset = News.objects.all()
+    serializer_class = NewsSerializer

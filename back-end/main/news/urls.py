025cc8610c671a0c645866ab65c18cb1e16c153d
Stelmaszv <stelmaszv@gmail.com>
2020-06-01 16:48:04 +0200

    remowe test

--- back-end/main/news/urls.py
+++ back-end/main/news/urls.py
@@ -2,7 +2,6 @@
 from django.urls import path, include
 from . import views
 from rest_framework import routers
-##dqd
 router = routers.DefaultRouter()
 router.register('news',views.Top_News)
 urlpatterns = [

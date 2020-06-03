4dbd933c34c64bcd8eada18164029e441bdb50ad
Stelmaszv <stelmaszv@gmail.com>
2020-06-03 12:49:42 +0200

    add model Category in models and for admin site

--- back-end/main/news/models.py
+++ back-end/main/news/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+class Category(models.Model):
+    name = models.CharField(max_length=50)
 class News(models.Model):
     title = models.CharField(max_length=50)
     photo = models.FileField(upload_to='img', null=True, blank=True)
     views = models.IntegerField(default=0)
+    category = models.ForeignKey(to='news.Category',on_delete=models.SET_NULL,null=True,blank=True,related_name='category')
     def __str__(self):
         return  self.title

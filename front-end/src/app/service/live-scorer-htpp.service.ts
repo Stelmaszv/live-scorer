664f6be712eddc49dde9b99b1bcb8dec4d442566
Stelmaszv <stelmaszv@gmail.com>
2020-06-09 16:56:51 +0200

    add method Get_Top_Competitions

--- front-end/src/app/service/live-scorer-htpp.service.ts
+++ front-end/src/app/service/live-scorer-htpp.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs'
+import { Competition_Model } from '../models/competitions'
 @Injectable({
   providedIn: 'root'
 })
 export class LiveScorerHtppService {
-
-  constructor() { }
+  competitions:string = 'http://127.0.0.1:8000/competitions/';
+  constructor(private http:HttpClient) { }
+  public Get_Top_Competitions() :Observable<Competition_Model[]> {
+    return this.http.get<Competition_Model[]>(this.competitions)
+  }
 }

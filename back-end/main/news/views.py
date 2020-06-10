2375cce61ac0cd6f55a34e10ec11714369c63d69
Stelmaszv <stelmaszv@gmail.com>
2020-06-10 16:20:03 +0200

    Add compattions seralizers

--- back-end/main/news/views.py
+++ back-end/main/news/views.py
@@ -6,6 +6,7 @@ class Top_News(viewsets.ModelViewSet):
     queryset = News.objects.all().order_by('-views')[:3]
     serializer_class = NewsSerializer
 class Get_news_from_category(API_prototype):
+    serializer_class = NewsSerializer
     def set_query_set(self):
         self.queryset = News.objects.filter(category__name=self.kwargs.get('category')).order_by('-views')[:7]
 class Get_news_from_category_all(Get_news_from_category):

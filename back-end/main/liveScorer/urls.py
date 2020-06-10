47fb595a4515ca2b3aedb6a68984a222d9f0f161
Stelmaszv <stelmaszv@gmail.com>
2020-06-10 15:46:25 +0200

    add compotition_serializer

--- back-end/main/liveScorer/urls.py
+++ back-end/main/liveScorer/urls.py
@@ -7,6 +7,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('',include(router.urls)),
+    path('get_competition_by_name/<str:competition>',views.Get_competition_by_name.as_view(),name='get_competition_by_name'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

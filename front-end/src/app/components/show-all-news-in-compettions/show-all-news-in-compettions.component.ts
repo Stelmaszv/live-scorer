1daa020e6906cab186dc68d8d0aa57112105e472
Stelmaszv <stelmaszv@gmail.com>
2020-06-12 15:33:06 +0200

    add ShowAllNewsInCompettionsComponent

--- front-end/src/app/components/show-all-news-in-compettions/show-all-news-in-compettions.component.ts
+++ front-end/src/app/components/show-all-news-in-compettions/show-all-news-in-compettions.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { PaginatorComponent } from '../paginator/paginator.component' 
 
 @Component({
   selector: 'app-show-all-news-in-compettions',
   templateUrl: './show-all-news-in-compettions.component.html',
   styleUrls: ['./show-all-news-in-compettions.component.scss']
 })
-export class ShowAllNewsInCompettionsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class ShowAllNewsInCompettionsComponent extends PaginatorComponent{
+ 
+  category:string
+  protected init() : void{
+    this.category = this.route.snapshot.paramMap.get('compettion');
+    this.get_pages='Get_news_from_category_pages/'+this.category
+    this.pagination_data_get_method=this.ns.Get_All_news_By_competition(this.category,this.page)
   }
 
+ 
 }
--- front-end/src/app/service/news.service.ts
+++ front-end/src/app/service/news.service.ts
@@ -2,8 +2,6 @@ import { Injectable } from '@angular/core';
 import { News_Model } from '../models/news';
 import { HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
-
-
 @Injectable({
   providedIn: 'root'
 })
@@ -11,6 +9,7 @@ export class NewsService {
   Top_News_url:string = 'http://127.0.0.1:8000/news/';
   From_category_url:string = 'http://127.0.0.1:8000/Get_news_from_category/';
   All_From_ctegory_url:string = 'http://127.0.0.1:8000/Get_news_from_category_all/';
+  Get_news_from_competitions_url='http://127.0.0.1:8000/Get_news_from_competitions/'
   constructor(private http:HttpClient) { }
   public Get_Top_News() :Observable<News_Model[]> {
     return this.http.get<News_Model[]>(this.Top_News_url)
@@ -20,5 +19,8 @@ export class NewsService {
   }
   public Get_All_News_From_Category(category,page) :Observable<News_Model[]> {
     return this.http.get<News_Model[]>(this.All_From_ctegory_url+''+category+'?page='+page)
+  }
+  public Get_All_news_By_competition(competition,page) :Observable<News_Model[]> {
+    return this.http.get<News_Model[]>(this.Get_news_from_competitions_url+''+competition+'?page='+page)
   }
 }

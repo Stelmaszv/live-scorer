5fdbce43acf94bece341235e144747067768c7f1
Stelmaszv <stelmaszv@gmail.com>
2020-06-12 15:22:56 +0200

    Clean code in "PaginatorComponent"

--- front-end/src/app/components/all-news-in-category/all-news-in-category.component.ts
+++ front-end/src/app/components/all-news-in-category/all-news-in-category.component.ts
@@ -1,5 +1,4 @@
 import { Component} from '@angular/core';
-import { News_Model } from  '../../models/news'
 import { PaginatorComponent } from '../paginator/paginator.component' 
 @Component({
   selector: 'app-all-news-in-category',
@@ -9,11 +8,8 @@ import { PaginatorComponent } from '../paginator/paginator.component'
 export class AllNewsInCategoryComponent extends PaginatorComponent {
   
   category:string
-  pagination_data:Array<News_Model>;
-
   protected init() : void{
     this.category = this.route.snapshot.paramMap.get('category');
-    this.page=1;
     this.get_pages='Get_news_from_category_pages/'+this.category
     this.pagination_data_get_method=this.ns.Get_All_News_From_Category(this.category,this.page)
   }
--- front-end/src/app/components/paginator/paginator.component.ts
+++ front-end/src/app/components/paginator/paginator.component.ts
@@ -4,6 +4,7 @@ import {NewsService} from '../../service/news.service'
 import { Pages_Service } from '../../service/pages.service'
 import { Pages_Model } from  '../../models/pages'
 import { HostListener } from '@angular/core';
+import { News_Model } from  '../../models/news'
 @Component({
   selector: 'app-paginator',
   templateUrl: './paginator.component.html',
@@ -15,35 +16,44 @@ export abstract class PaginatorComponent implements OnInit {
   pages:Pages_Model;
   loding:Boolean=false;
   pagination_data_get_method
-  list_news:Array<any>;
-  pagination_data;
+  list:Array<any>;
+  pagination_data:Array<News_Model>;
   page:number;
   constructor(protected ns:NewsService, protected route: ActivatedRoute,protected Pages_Service:Pages_Service) { }
   ngOnInit(): void {
+    this.page=1;
     this.init()
+    this.list=[]
     this.get_data(this.page)
-    this.list_news=[]
+    this.check()
   }
   protected abstract init() : void;
-  protected get_data(page){
+
+  @HostListener("window:scroll", []) onWindowScroll() {
+    this.scrool_evant()
+  }
+  protected get_data(page) : void 
+  {
     this.loding_click=true
     this.Pages_Service.get_pages(this.get_pages,page).subscribe(pages => {
        this.pages=pages
-       if (this.pages.valid){
+       if (pages.valid){
           this.loding=true
           this.pagination_data_get_method.subscribe(news => {
           for (let item of news ){
-            this.list_news.push(item)
+            this.list.push(item)
           }
-          this.pagination_data=this.list_news
+          this.pagination_data=this.list
           this.loding=false;
           this.loding_click=false;
         });
       }
     });
+
   }
   
-  protected scrool_evant(){
+  protected scrool_evant() : void 
+  {
     const verticalOffset = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0
     const max = Math.max( 
       document.body.scrollHeight, 
@@ -52,15 +62,34 @@ export abstract class PaginatorComponent implements OnInit {
       document.documentElement.scrollHeight, 
       document.documentElement.offsetHeight 
     );
+
     const max_procent=5*max/100 
     if(verticalOffset>max_procent){
       if (this.pages.valid){
-          if(this.loding_click==false){
-            this.get_data(this.page)
+          if(!this.loding){
+            this.get_more()
             document.body.scrollTop=0
           }
       }
     }
   }
 
+  private get_more() : void 
+  {
+    this.page=this.page+1
+    this.get_data(this.page)
+  }
+
+  private check() : void 
+  {
+    if (this.get_pages == undefined){
+      console.log('this.get_pages is undefined')
+    }
+    if (this.pagination_data_get_method == undefined){
+      console.log('this.pagination_data_get_method is undefined')
+    }
+    if(typeof this.pagination_data_get_method != 'object'){
+      console.log('valule must method in class')
+    }
+  }
 }

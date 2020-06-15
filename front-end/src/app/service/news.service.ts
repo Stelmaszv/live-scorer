1ce98e47c6833fc86e6774b03fcd833b2abfa34b
Stelmaszv <stelmaszv@gmail.com>
2020-06-15 17:46:24 +0200

    add top lists in on-top-component

--- front-end/src/app/service/news.service.ts
+++ front-end/src/app/service/news.service.ts
@@ -11,7 +11,7 @@ export class NewsService {
   Get_News_url:string = 'http://127.0.0.1:8000/Get_News/';
   From_category_url:string = 'http://127.0.0.1:8000/Get_news_from_category/';
   All_From_ctegory_url:string = 'http://127.0.0.1:8000/Get_news_from_category_all/';
-  Get_news_from_competitions_url='http://127.0.0.1:8000/Get_news_from_competitions/'
+  Get_news_from_competitions_url='http://127.0.0.1:8000/Get_top_news_in_Competitions/'
   Get_top_news_in_Category_url='http://127.0.0.1:8000/Get_top_news_in_Category/'
   constructor(private http:HttpClient) { }
   public Get_Top_News() :Observable<News_Model[]> {

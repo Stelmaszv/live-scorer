fb5318f057d3922bebb613b789f603a549ea0e65
Stelmaszv <stelmaszv@gmail.com>
2020-06-17 14:33:23 +0200

    update coments component

--- front-end/src/app/components/get-news/coments/coments.component.ts
+++ front-end/src/app/components/get-news/coments/coments.component.ts
@@ -11,12 +11,12 @@ export class ComentsComponent extends PaginatorComponent {
   pagination_data:Array<Coments_Model>;
   protected init() : void{
     this.get_pages='Get_Coments_pages/'+this.id
-    this.pagination_data_get_method=this.ns.Get_Coments_service(this.id,1)
+    this.pagination_data_get_method=this.ns.Get_Coments_service(this.id,this.page)
   }
   protected get_more() : void 
   {
     this.page=this.page+1
-    this.pagination_data_get_method=this.ns.Get_Coments_service(this.id,this.page)
+    this.pagination_data_get_method=this.ns.Get_All_News_From_Category(this.category,this.page)
     this.get_data(this.page)
   }
 }

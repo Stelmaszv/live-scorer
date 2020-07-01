afcbc824caa11d758da4ef33a7964b45c8202a42
Stelmaszv <stelmaszv@gmail.com>
2020-07-01 15:03:43 +0200

    clean code in core.auth and prototype.classes

--- back-end/main/core/auth.py
+++ back-end/main/core/auth.py
@@ -1,11 +1,12 @@
 from rest_framework.authentication import BasicAuthentication
-from django.contrib.auth import authenticate, get_user_model
+from django.contrib.auth import \
+    authenticate, \
+    get_user_model
 class Authentication(BasicAuthentication):
+
     def authenticate(self, request):
-        # Get the username and password
         username = request.data.get('username', None)
         password = request.data.get('password', None)
-
         credentials = {
             get_user_model().USERNAME_FIELD: username,
             'password': password
--- back-end/main/core/prototype_classes.py
+++ back-end/main/core/prototype_classes.py
@@ -1,17 +1,22 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import  APIView
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.paginator import \
+    Paginator, \
+    EmptyPage, \
+    PageNotAnInteger
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .auth import Authentication
 class CastumPaginator:
+
     def __init__(self,query,on_page,request):
         self._queryset=query;
         self._on_page=on_page;
         self._request=request
         self._page = request.GET.get('page', 1)
         self._setPage()
+
     def _setPage(self):
         paginator = Paginator(self._queryset, self._on_page)
         try:
@@ -21,30 +26,44 @@ class CastumPaginator:
         except EmptyPage:
             self._queryset = paginator.page(paginator.num_pages)
         self._paginator_obj = paginator
+
     def return_query_set(self):
+
         return self._queryset
+
     def paginator_obj(self):
+
         return self._paginator_obj
+
     def return_page(self):
+
         return self._page
+
     def return_num_pages(self):
+
         return self._paginator_obj.num_pages
+
 class API_prototype(APIView):
+
     on_page=None
     return_pages=False
     many=True
     if_auth=False
     authentication_classes = (SessionAuthentication, Authentication,)
     permission_classes = [IsAuthenticatedOrReadOnly]
+
     def paginate(self, request, *args, **kwargs):
+
         if self.on_page is not None:
             paginator=CastumPaginator(self.queryset,self.on_page,request)
             self.queryset = paginator.return_query_set()
             self.paginator_obj = paginator
             self.page=paginator.return_page()
             self.num_pages=paginator.return_num_pages()
+
     def set_query_set(self,request):
         pass
+
     def post(self, request, *args, **kwargs):
         self.set_query_set(request)
         serializer = self.serializer_class(data=request.data,many=False)
@@ -53,19 +72,23 @@ class API_prototype(APIView):
             self.paginate(request)
             return self.return_respanse(request)
         return self.return_respanse(request)
+
     def _API_get(self, request, *args, **kwargs):
         self.auth=False
         serializer = self.serializer_class(self.queryset, many=self.many)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
+
     def get(self, request, *args, **kwargs):
         self.permission_classes = []
         self.set_query_set(request)
         self.paginate(request)
         return self.return_respanse(request)
+
     def return_respanse(self,request):
         if self.return_pages:
             return self._return_pages_json(request)
         return self._API_get(request)
+
     def _return_pages_json(self,request):
         pages={
             'valid':self._validPages(self.num_pages,int(self.page)),
@@ -73,17 +96,22 @@ class API_prototype(APIView):
             'page':int(self.page)
         }
         return Response(data=pages, status=status.HTTP_200_OK)
+
     def _validPages(self,max,page):
         if page > max:
             return False;
         return True
+
 class API_prototype_get(APIView):
+
     def get(self, request, *args, **kwargs):
         self.set_query_set(request)
         return self.API_get(request)
+
     def API_get(self, request, *args, **kwargs):
         serializer = self.serializer_class(self.queryset)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
+
     def set_query_set(self,request):
         pass
 
